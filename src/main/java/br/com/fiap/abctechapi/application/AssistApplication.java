02b9e52c1db7aeeed61af4e28e81e31091725237
Willians Dortt <rm346348@fiap.com.br>
2023-02-10 15:13:32 -0300

    Application Assists + Controller

---
+++ src/main/java/br/com/fiap/abctechapi/application/AssistApplication.java
@@ -0,0 +1,11 @@
+package br.com.fiap.abctechapi.application;
+
+import br.com.fiap.abctechapi.application.dto.AssistDto;
+
+import java.util.List;
+
+public interface AssistApplication {
+
+    List<AssistDto> getAssists();
+
+}
